840e3e423138a8c3404228adb055b1e4d98d258c
kennie-larkson <materialkenny@gmail.com>
2021-08-26 14:15:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BasicReg from "./components/basic-reg.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">This is your custom React app.</div>
+      <BasicReg />
+    </>
   );
 }
+
+
 
 export default App;
